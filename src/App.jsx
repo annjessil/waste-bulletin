7b22f2a001ba7f3c09d285fc9b8bd50354ecc66f
annjessil <annmariejessil8@gmail.com>
2025-05-19 00:35:14 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import { useState } from 'react'
-import { Routes, Route, HashRouter, Navigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import 'react'
+
 import NavBar from './components/NavBar.jsx'
 import Bulletin from './pages/Bulletin.jsx'
 import Home from './pages/Home.jsx'
@@ -9,16 +11,15 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <HashRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/bulletin" element={<Bulletin />} />
-        <Route path="/search" element={<Search />} />
-        {/* Redirect any unknown path to Home */}
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </HashRouter>
+    <BrowserRouter>
+        <NavBar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/bulletin" element={<Bulletin/>}/>
+          <Route path="/search" element={<Search/>}/>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+    </BrowserRouter>
   )
 }
 
