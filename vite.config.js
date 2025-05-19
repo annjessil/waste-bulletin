8c5a203db7645905687b413598e5e92058ed8cb2
annjessil <annmariejessil8@gmail.com>
2025-05-19 00:16:24 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/waste-bulletin/', // This matches your repo name exactly
+  base: '/waste-bulletin/',
+  publicDir: 'public' // This ensures public assets are copied correctly
 })
